35faab2b06e1bab2c533507d831b567684003b0d
Josh Clemons <jclemons@denniskirk.com>
2023-08-01 09:27:13 -0500

    Added csv download for league picks

--- src/contexts/CompetitorContext.jsx
+++ src/contexts/CompetitorContext.jsx
@@ -20,7 +20,6 @@ export const CompetitorProvider = ({children}) => {
                 'Authorization': `Bearer ${user.accessToken}`
             }
         })
-        console.log(data);
         return data;
     }
 
