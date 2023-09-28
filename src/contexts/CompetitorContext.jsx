e18a5fa6346b0ec34bf9264abb77d1cbbcc0ae3f
Josh Clemons <jclemons@denniskirk.com>
2023-09-28 07:45:07 -0500

    updated overview to select current week's games

--- src/contexts/CompetitorContext.jsx
+++ src/contexts/CompetitorContext.jsx
@@ -1,21 +1,22 @@
-// Libraries
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import {createContext, useContext} from 'react';
-import {useQuery} from 'react-query';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 
-// Contexts
-import {UserContext} from './UserContext';
+import { UserContext } from './UserContext';
 
 export const CompetitorContext = createContext();
 
-export const CompetitorProvider = ({children}) => {
+export const CompetitorProvider = ({ children }) => {
     // Contexts
-    const {user} = useContext(UserContext);
+    const { user } = useContext(UserContext);
+
+    // State to track if the user has signed in successfully
+    const [hasSignedIn, setHasSignedIn] = useState(false);
 
     // Functions
     const fetchCompetitors = async () => {
-        const {data} = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/gamedata/competitors`, {
+        const { data } = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/gamedata/competitors`, {
             headers: {
                 'Authorization': `Bearer ${user.accessToken}`
             }
@@ -24,12 +25,21 @@ export const CompetitorProvider = ({children}) => {
     }
 
     // Query
-    const {data: competitors, isError, error, isLoading, refetch} = useQuery('competitors', fetchCompetitors, {
+    const { data: competitors, isError, error, isLoading, refetch } = useQuery('competitors', fetchCompetitors, {
         refetchOnWindowFocus: false,
+        enabled: hasSignedIn, // Only fetch when hasSignedIn is true
     });
 
+    // Use useEffect to set hasSignedIn when signIn is successful
+    useEffect(() => {
+        if (user && user.accessToken) {
+            setHasSignedIn(true);
+        }
+    }, [user]);
+
+
     return (
-        <CompetitorContext.Provider value={{competitors, isError, error, isLoading, refetch}}>
+        <CompetitorContext.Provider value={{ competitors, isError, error, isLoading, refetch }}>
             {children}
         </CompetitorContext.Provider>
     )
