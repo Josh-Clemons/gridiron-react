b38a2baef8136109bc0511dc60f0620ee2f78529
Josh Clemons <jclemons@denniskirk.com>
2023-10-12 07:52:32 -0500

    Reformatted csv download

--- src/components/Buttons/DownloadPicksButton.jsx
+++ src/components/Buttons/DownloadPicksButton.jsx
@@ -1,18 +1,42 @@
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
 import Papa from 'papaparse';
 import PropTypes from 'prop-types';
 
-const DownloadPicksButton = ({picks}) => {
-    const simplifiedPicks = picks.map(pick => ({
-        user: pick.owner.username,
-        week: pick.week,
-        value: pick.value,
-        team: pick.competitor?.team?.abbreviation
-    }));
+const DownloadPicksButton = ({ picks }) => {
+    // Sort the picks array as per your requirements
+    const sortedPicks = picks.sort((a, b) => {
+        if (a.owner.username < b.owner.username) return -1;
+        if (a.owner.username > b.owner.username) return 1;
+        if (a.week < b.week) return -1;
+        if (a.week > b.week) return 1;
+        if (a.value > b.value) return -1;
+        if (a.value < b.value) return 1;
+        return 0;
+    });
 
+    const userDataMap = new Map();
+
+    sortedPicks.forEach(pick => {
+        const user = pick.owner.username;
+        const week = pick.week;
+        const value = pick.value;
+        const abbreviation = pick.competitor?.team?.abbreviation;
+
+        if (!userDataMap.has(user)) {
+            userDataMap.set(user, { username: user });
+        }
+
+        userDataMap.get(user)[`Week ${week} Value ${value}`] = abbreviation;
+    });
+
+    const userDataArray = Array.from(userDataMap.values());
+
+    // Convert userDataArray to CSV format
+    const csvContent = Papa.unparse(userDataArray);
+
+    // Function to download the CSV
     const handleDownload = () => {
-        const csvContent = Papa.unparse(simplifiedPicks);
         const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
         const url = URL.createObjectURL(blob);
 
@@ -31,11 +55,15 @@ const DownloadPicksButton = ({picks}) => {
     };
 
     return (
-        <Button color="success" onClick={handleDownload}
-                sx={{m: 1, borderWidth: 2,
-                    alignSelf: 'flex-end',
-                    '&:hover': {borderWidth: '2px'},
-                }}
+        <Button
+            color="success"
+            onClick={handleDownload}
+            sx={{
+                m: 1,
+                borderWidth: 2,
+                alignSelf: 'flex-end',
+                '&:hover': { borderWidth: '2px' },
+            }}
         >
             League Picks
             <DownloadIcon />
